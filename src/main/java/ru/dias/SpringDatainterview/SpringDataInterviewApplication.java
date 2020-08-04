7d9b02758e6a4f7e055b43ed8d1bade13d920a19
Dmitrenko <Dias64@mail.ru>
2020-08-04 19:31:11 +0300

    init SpringData-interview

---
+++ src/main/java/ru/dias/SpringDatainterview/SpringDataInterviewApplication.java
@@ -0,0 +1,13 @@
+package ru.dias.SpringDatainterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataInterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataInterviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/dias/SpringDatainterview/SpringDataInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dias.SpringDatainterview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataInterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
